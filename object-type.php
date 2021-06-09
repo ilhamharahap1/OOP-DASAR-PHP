6036a5b66f6babf37b3f7c201ccba5f656877ec6
ilhamharahap1 <ilhamharahap59@gmail.com>
2021-06-09 10:07:16 +0700

    Object type menambah class baru dan menambahkan function cetak dan class

--- object-type.php
+++ object-type.php
@@ -25,17 +25,26 @@ class Produk{
 }
 
 
+
+class CetakInfoProduk{
+	public function cetak( Produk $produk ){ // fungsi cetak hanya menerima parameter jenis kelas produk dengan objek apa, ketika berusaha mencetak instan kelas produk yg tidak ada, akan error, harus kelas yang ada
+		$str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})"; //getLabel disini adlah function getLabel yg di buat
+		return $str;
+	}
+}
+
+
+
+
+
 $produk1 = new Produk("naruto", "masasi","shonen", 30000); 
-
-
-
 
 echo "<hr>";
 
 $produk2 = new Produk("uncharted", "neil", "sony", 250000);
-$produk3 = new Produk("smacke down");
 echo "Komik :". $produk1->getLabel();
 echo "<br>";
 echo "Game :". $produk2->getLabel();
 echo "<br>";
-var_dump($produk3); 
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk1); //jalnkan function dengan kelas produk1 komik yang telah di buat
