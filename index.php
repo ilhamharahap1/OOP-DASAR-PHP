8a46512dff3b6230fdbe5a23b03a175d88c20aa9
ilhamharahap1 <ilhamharahap59@gmail.com>
2021-06-09 08:43:11 +0700

    menambah data

--- index.php
+++ index.php
@@ -15,15 +15,15 @@ class Produk{
 
 }
 
-$produk1 = new produk(); 
-//var_dump($produk1); ini kan mencetak nilai default, yaitu judul = judul
-$produk1->judul = "naruto";
-var_dump($produk1); //ini akan menimpa nilai default, dari judul akan menjadi naruto
+// $produk1 = new produk(); 
+// //var_dump($produk1); ini kan mencetak nilai default, yaitu judul = judul
+// $produk1->judul = "naruto";
+// var_dump($produk1); //ini akan menimpa nilai default, dari judul akan menjadi naruto
 
-$produk2 = new Produk();
-$produk2->judul = "warkop"; //ini akan mencetak dua produk, dimana produk ke dua di timpa dengan warkop
-$produk2->tambahProperty = "hahahah"; //karena tidak ada properti bernama ini, makan akan di tambahkan properti bari dengan nilai hahaha
-var_dump($produk2);
+// $produk2 = new Produk();
+// $produk2->judul = "warkop"; //ini akan mencetak dua produk, dimana produk ke dua di timpa dengan warkop
+// $produk2->tambahProperty = "hahahah"; //karena tidak ada properti bernama ini, makan akan di tambahkan properti bari dengan nilai hahaha
+// var_dump($produk2);
 
 
 
@@ -37,3 +37,14 @@ var_dump($produk2);
 //mempresentasikan perilaku dari sebuah objek
 //function yg ada didalam pbject
 //samaseperti function di dalam php, di tambha visibility di depan nya
+
+
+
+$produk3 = new Produk();
+$produk3->judul = "naruto";
+$produk3->penulis = "masashi khisimo";
+$produk3->peneerbit = "shonen jump";
+$produk3->harga = 30000;
+
+
+echo "Komik : $produk3->penulis, $produk3->peneerbit";
