a5eb3404153c805ee36af6ddf9b295e0e7450c69
ilhamharahap1 <ilhamharahap59@gmail.com>
2021-06-09 10:14:44 +0700

    pengenalan inheritance/pewarisan

---
+++ inheritance.php
@@ -0,0 +1,6 @@
+<?php  
+
+
+//inheritance/pewarisan konsep yg menciptakan hirarti antar kelas, memungkinkan menghubungkan antar kelas
+//child class , ,mewarisi semua properti dan method dari parent nya(yg visible)
+//child class, memperluas(extend) fungsionalitas dari parent nya
